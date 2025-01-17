0c3f50a9e0ec112a48b9313ff7662c22477d3d49
kegoba <egobakelvin@gmail.com>
2025-01-17 22:36:05 +0100

    modifying the migation script

--- migratescript.sh
+++ migratescript.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-if python manage.py showmigrations | grep '\[ \]'; then
-    echo "Running migrations..."
-    python manage.py migrate
+
+
+if python manage.py makemigrations --dry-run | grep "No changes detected"; then
+    echo "No new migrations to apply."
 else
-    echo "Migrations already applied."
+    echo "Making migrations..."
+    python manage.py makemigrations
 fi
+
+
+echo "Running migrations..."
+python manage.py migrate
 
 
 echo "Starting Gunicorn server..."
 exec gunicorn inventory.wsgi:application --bind 0.0.0.0:8000
+
